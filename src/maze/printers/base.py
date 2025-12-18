ab68676626727a9dbef356d1c130baed7d01ed40
Muhammad Ilyasa Fadhlih <muhammadilyasafadhlih@gmail.com>
2025-12-18 16:55:50 +0700

    Add abstractmethod property to base class

--- src/maze/printers/base.py
+++ src/maze/printers/base.py
@@ -1,6 +1,8 @@
 from ..grid import Grid
+from abc import abstractmethod
 
 
 class MazePrinter:
+    @abstractmethod
     def Print(self, grid: Grid) -> None:
-        raise NotImplementedError()
+        pass
